1443e394a818e41af56a73e1005f3a2e31deac55
yangdp1983 <741744094@qq.com>
2020-09-06 20:41:44 +0800

    添加system.out.println

--- src/main/java/com/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/springbootdemo/SpringbootdemoApplication.java
@@ -8,6 +8,8 @@ public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
+		System.out.println("springbootdemo hello");
+
 	}
 
 }
